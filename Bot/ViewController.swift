abe8535c3138b6b12874c44e1541e9a8ed9b7c45
Theodrosrun <theodros.mulugeta@creatis.ch>
2019-02-21 08:26:24 +0100

    Initial Commit

---
+++ Bot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bot
+//
+//  Created by Theodros Mulugeta on 21.02.19.
+//  Copyright © 2019 Theodros Mulugeta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
